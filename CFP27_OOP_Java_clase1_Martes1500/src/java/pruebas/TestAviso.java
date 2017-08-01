f31117ada1b9ac00b531983c53f4c41c0823c6b7
educacion <educacion@localhost>
2017-08-01 19:47:04 +0000

    Martes, a las 15, java web OOP, primera clase, o clase 1

---
+++ CFP27_OOP_Java_clase1_Martes1500/src/java/pruebas/TestAviso.java
@@ -0,0 +1,43 @@
+package pruebas;
+
+import entidades.Aviso;
+
+/**
+ * <pre>
+ *  TODO: Clases
+ *  TODO: Objetos e Instancias
+ *  TODO: atributos
+ *  TODO: metodos
+ *  TODO: public, privado
+ *  TODO: Herencia
+ *  TODO: Interfaces
+ *  TODO: Excepciones
+ *  TODO: JDBC, SQL, Base de Datos, conectar
+ *  TODO: Agregacion
+ *  TODO: Composicion
+ *  TODO: HTTPServlet, web
+ *  TODO: Filtros 
+ * </pre>
+ * 
+ * @author educacion
+ */
+public class TestAviso {
+    
+    public static void main( String [] args){
+        System.out.println("Test Aviso: [..]");
+        
+        Aviso campera = new Aviso();
+        campera.titulo = "Campera Deportiva";
+        campera.precio = 2000;
+        
+        Aviso pantalon = new Aviso();
+        pantalon.titulo = "Pantalon Dry Fit";
+        pantalon.precio = 1000;
+        
+        System.out.println( campera );
+        System.out.println( pantalon );
+        
+        System.out.println("Test Aviso: [OK]");
+    }
+    
+}
