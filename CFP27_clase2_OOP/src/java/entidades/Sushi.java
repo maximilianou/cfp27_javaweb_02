7ab2f519bc56914239166f5e4ab31b44d26cc3e2
educacion <educacion@localhost>
2017-08-04 19:39:24 +0000

    Agregando Suchi

--- CFP27_clase2_OOP/src/java/entidades/Sushi.java
+++ CFP27_clase2_OOP/src/java/entidades/Sushi.java
@@ -12,11 +12,21 @@ package entidades;
 public class Sushi {
     public String nombre;
     
+    /** La cantidad de piezas Esta acotada por el tipo de dato
+     * <a href='https://docs.oracle.com/javase/7/docs/api/java/lang/Byte.html'>
+     * Tipo de dato Byte
+     * </a>
+     */
     public byte cantidaPiezas;
     short coloreo;
     int stock;
     long cantdadDeGranosdeArroz;
     
+    /** El precio se rige por la definicion de Float
+     * <a href='https://docs.oracle.com/javase/7/docs/api/java/lang/Float.html'>
+     * Tipo de dato Float
+     * </a>
+     */
     public float precio;
     double cantidadMateriaGrasa;
     
