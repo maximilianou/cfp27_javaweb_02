d4e013fa3cb69a598c17b337ab33d57a6c0bf004
educacion <educacion@localhost>
2017-08-04 20:04:13 +0000

    inicio ejercicio sushi

---
+++ CFP27_clase2_OOP/src/java/ejercicio/pruebas/TestDelivery.java
@@ -0,0 +1,24 @@
+package ejercicio.pruebas;
+
+/**
+ * <pre>
+ * Ejercicio: Tipos basicos de datos, en carar solucion real
+ *   El Delivery es un Servicio de Envio
+ *   Un Servicio es la Venta
+ *   El Cliente tiene Telefono
+ *   El Envio es a un Cliente
+ *   El Delivery viene por apliccion y sabe la ubicacion 
+ *   La Venta esta compuesta por
+ *      Cliente,
+ *      Envio desde Aplicacion, o por telefono, al local
+ *      
+ * </pre>
+ * @author educacion
+ */
+public class TestDelivery {
+    public static void main(String [] args){
+        System.out.println("Test Compra [..]");
+        
+        System.out.println("Test Compra [OK]");
+    }
+}
