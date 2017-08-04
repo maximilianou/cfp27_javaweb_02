75e3623887a954413d02223ade6665c2cc2543de
educacion <educacion@localhost>
2017-08-04 19:19:56 +0000

    Ejercicio basico Segun Sergio

---
+++ CFP27_clase2_OOP/src/java/entidades/Sushi.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author educacion
+ */
+public class Sushi {
+    public String nombre;
+    
+    public byte cantidaPiezas;
+    short coloreo;
+    int stock;
+    long cantdadDeGranosdeArroz;
+    
+    public float precio;
+    double cantidadMateriaGrasa;
+    
+    boolean frio;
+    char presentacion;
+}
