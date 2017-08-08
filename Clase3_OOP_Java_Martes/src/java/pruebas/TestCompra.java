f4f4b251d6f842d137e33a2f3e3a765e486075b2
educacion <educacion@localhost>
2017-08-08 15:01:07 +0000

    Lista de productos en Compra

--- Clase3_OOP_Java_Martes/src/java/pruebas/TestCompra.java
+++ Clase3_OOP_Java_Martes/src/java/pruebas/TestCompra.java
@@ -8,18 +8,41 @@ import entidades.*;
  * @author educacion
  */
 public class TestCompra {
+
     public static void main(String[] args) {
         System.out.println("pruebas.TestCompra.main() [..]");
-        
+
         Compra pedidoArepas = new Compra();
         pedidoArepas.elCliente = new Cliente();
+        pedidoArepas.elCliente.nombre = "Alberto";
         pedidoArepas.elVendedor = new Vendedor();
-        System.out.println( pedidoArepas );
+        pedidoArepas.elVendedor.nombre = "Michael Angelo";
+        Producto laPe = new Producto();
+        laPe.nombre = "peluda";
+        laPe.precio = 90;
+        Producto laCh = new Producto();
+        laCh.nombre = "ja queso";
+        laCh.precio = 70;
+        pedidoArepas.agregar(laPe);
+        pedidoArepas.agregar(laCh);
+        System.out.println(pedidoArepas);
 
         Compra pedidoPizza = new Compra();
-        pedidoPizza.elCliente = new Cliente();
-        pedidoPizza.elVendedor = new Vendedor();
-        System.out.println( pedidoPizza );
+
+        Cliente elClientito = new Cliente();
+        elClientito.nombre = "Rogelio";
+        pedidoPizza.elCliente = elClientito;
+
+        Vendedor elVendedorcito = new Vendedor();
+        elVendedorcito.nombre = "Carlos";
+        pedidoPizza.elVendedor = elVendedorcito;
+        Producto muza = new Producto();
+        muza.nombre = "Muzzarella ";
+        Producto fuga = new Producto();
+        fuga.nombre = "Fugazzeta Rellena con Jamon y Queso y Morrones, y Palmeitos";
+        pedidoPizza.agregar(fuga);
+        pedidoPizza.agregar(muza);
+        System.out.println(pedidoPizza);
 
         System.out.println("pruebas.TestCompra.main() [OK]");
     }
