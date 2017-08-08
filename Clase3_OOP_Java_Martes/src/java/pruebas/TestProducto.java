5d69fd0c3fde0e68b76e6adb7fe35d50d1c60a47
educacion <educacion@localhost>
2017-08-08 13:03:23 +0000

    Progrmacion por Testing basico, de Producto

---
+++ Clase3_OOP_Java_Martes/src/java/pruebas/TestProducto.java
@@ -0,0 +1,33 @@
+package pruebas;
+
+import entidades.*;
+
+/**
+ * <pre>
+ * Testeo unitrio de Producto
+ * Aqui podemos ver, el Standad Error y Standard Output
+ * Que salen en la interface grafica diferido, indiscriminado.
+ * 
+ * </pre>
+ * @author educacion
+ */
+public class TestProducto {
+    public static void main(String[] args) {
+        System.out.println("Test Productos [..]");
+        
+        Producto peluda = new Producto();
+        peluda.nombre = "Arepa Peluda";
+        peluda.precio = 80;
+        
+        Producto cheese = new Producto();
+        cheese.nombre = "Arepa Jamon y Queso";
+        cheese.precio = 70;
+
+        System.out.println("");
+        
+        
+        System.out.println("Test Productos [OK]");
+        //System.err.println("Test Productos [..]");
+        //System.err.println("Test Productos [OK]");
+    }
+}
