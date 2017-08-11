c196b9bfca238f95b949067e53fa977c8477b169
educacion <educacion@localhost>
2017-08-11 19:26:20 +0000

    Collecciones de a poco

--- Clase4_OOP_Java_Jose/src/java/pruebas/TestBebida.java
+++ Clase4_OOP_Java_Jose/src/java/pruebas/TestBebida.java
@@ -8,6 +8,7 @@ import java.util.*;
  * https://en.wikipedia.org/wiki/Test-driven_development
  * https://en.wikipedia.org/wiki/Behavior-driven_development
  * http://www.seleniumhq.org/
+ * http://www.oracle.com/technetwork/java/javase/downloads/index.html
  * 
  * 
  * </pre>
@@ -17,7 +18,8 @@ public class TestBebida {
     
     public static void main(String[] args) {
         System.out.println("pruebas.TestBebida.main() [..]");
-        ArrayList<Bebida> listaBebidas = new ArrayList();
+        //ArrayList<Bebida> listaBebidas = new ArrayList();
+        TreeSet<Bebida> listaBebidas = new TreeSet();
         
         Bebida ron = new Bebida();
         ron.nombre = "Ron Havana";
@@ -30,8 +32,22 @@ public class TestBebida {
         Bebida te = new Bebida();
         te.nombre = "Tequila Jose Cuervo";
         listaBebidas.add(te);
+        listaBebidas.add(te);
+        listaBebidas.add(te);
+        listaBebidas.add(te);
 
         System.out.println( listaBebidas );
+        
+        for( int indice = 0; indice < listaBebidas.size(); indice++ ){
+            System.out.print( " :"+ indice + "::" + listaBebidas.get(indice) );
+        }
+        System.out.println("");
+        
+        Iterator recorrer = listaBebidas.iterator();
+        while( recorrer.hasNext() ){
+            System.out.print( " =" + recorrer.next() );
+        }
+        System.out.println("");
         
         System.out.println("pruebas.TestBebida.main() [OK]");
     }
