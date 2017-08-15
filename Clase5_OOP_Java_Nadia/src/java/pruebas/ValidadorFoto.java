4af9ea47db1bb395a083d4c4181f827cbb2c9b4e
educacion <educacion@localhost>
2017-08-15 21:05:02 +0000

    Excepciones Caso Base Floreria

---
+++ Clase5_OOP_Java_Nadia/src/java/pruebas/ValidadorFoto.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebas;
+
+/**
+ *
+ * @author educacion
+ */
+class ValidadorFoto {
+
+    void luminosidad(Foto unaFoto) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (unaFoto.luz > -3 || unaFoto.luz  < 3 ){
+        }else{
+        }
+    }
+
+    void gradoEnfoque(Foto unaFoto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void nivelColor(Foto unaFoto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
