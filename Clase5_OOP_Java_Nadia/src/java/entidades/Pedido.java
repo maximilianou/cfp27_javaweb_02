4c1035a420c21f4e9993fc9eae9209743c91c1a8
educacion <educacion@localhost>
2017-08-15 18:56:56 +0000

    Excepciones Caso Base Floreria

---
+++ Clase5_OOP_Java_Nadia/src/java/entidades/Pedido.java
@@ -0,0 +1,15 @@
+package entidades;
+
+/**
+ *
+ * @author educacion
+ */
+public class Pedido {
+
+    public String nombre;
+
+    public void agregar(Flor centro) {
+        throw new UnsupportedOperationException("Aun no esta Programado."); 
+    }
+    
+}
