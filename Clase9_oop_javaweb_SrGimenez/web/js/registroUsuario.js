70f7895f4d5319610c2f32846236d9f907b9b2e2
educacion <educacion@localhost>
2017-08-29 20:05:46 +0000

    tomando datos del formulario

--- Clase9_oop_javaweb_SrGimenez/web/js/registroUsuario.js
+++ Clase9_oop_javaweb_SrGimenez/web/js/registroUsuario.js
@@ -1,9 +1,23 @@
 PET = {};
-
 PET.altaUsuario = function(){
+    //alert("Dando de Alta un Registro de Usuario.!!!");
+    /** Selectores de CSS
+     * https://www.w3schools.com/cssref/css_selectors.asp */
+    var losInput = 
+        document.querySelectorAll("#form_registro_usuario input");
+//        document.querySelectorAll(
+//          "#form_registro_usuario input[type=text], "
+//        + "#form_registro_usuario input[type=password] ");
+    var panelOK = document.querySelector("#panelOK");
+    var panelERROR = document.querySelector("#panelERROR");
+    for( var indice = 0; indice < losInput.length ; indice++ ){
+        losInput[indice].nombre = losInput[indice].name;
+        losInput[indice].valor  = losInput[indice].value;
+    }
     
-    alert("Dando de Alta un Registro de Usuario.!!!");
     
+    panelOK.innerHTML = JSON.stringify( losInput );
+    
+    //panelERROR.innerHTML = JSON.stringify( losInput );
 };
-
-alert("Estoy queriendo dar de alta usuarios!");
+//alert("Estoy queriendo dar de alta usuarios!");
