9da892853c047266f61773da108e83d95e094b25
educacion <educacion@localhost>
2017-09-05 18:53:23 +0000

    Agregando Estrurtura para JSON corchete y llvaaes

--- Clase11_oop_javaweb_Josesito/src/java/presentacion/VideoJuego.java
+++ Clase11_oop_javaweb_Josesito/src/java/presentacion/VideoJuego.java
@@ -1,20 +1,29 @@
 package presentacion;
 
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.TreeMap;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 @WebServlet(name = "VideoJuego", urlPatterns = {"/VideoJuego"})
 public class VideoJuego extends HttpServlet {
+    private Gson convertir = new Gson();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-         response.getWriter().println("GET, Metodo aun No Implementado");
+        ArrayList<TreeMap> listadoResultado = new ArrayList();
+
+        response.getWriter().println( 
+                 convertir.toJson( listadoResultado )
+         );
     }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
          response.getWriter().println("POST, Metodo aun No Implementado");
 
     }
