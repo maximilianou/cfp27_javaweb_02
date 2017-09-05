f663645d5b8a3ba6504d714e94f8fcd8c0499867
educacion <educacion@localhost>
2017-09-05 13:57:08 +0000

    Devolviendo al navegadodr JSON, con lista de objetos., Ahora faltaria acceder a la Base de DAtos, como proimo paso

--- Clase11_oop_javaweb_Levet/src/java/presentacion/ConSearch.java
+++ Clase11_oop_javaweb_Levet/src/java/presentacion/ConSearch.java
@@ -1,21 +1,41 @@
 package presentacion;
 
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.TreeMap;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 @WebServlet(name = "ConSearch", urlPatterns = {"/ConSearch"})
 public class ConSearch extends HttpServlet {
-
+    Gson convertir = new Gson();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        ArrayList<TreeMap> listaResultado = new ArrayList();
+        
+        TreeMap<String, String> unConcertista = new TreeMap();
+        unConcertista.put("nombre","Luka");
+        unConcertista.put("temple","Suave");
+        
+        TreeMap<String, String> otroConcertista = new TreeMap();
+        otroConcertista.put("nombre","Stjepan");
+        otroConcertista.put("temple","Fuerte");
+        
+        listaResultado.add(unConcertista);
+        listaResultado.add(otroConcertista);
+        
+        response.getWriter().println( 
+                convertir.toJson( listaResultado )
+        );
+        
     }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        response.getWriter().println("POST, No implementado AUN.");
     }
 }
