4018c29324c3a6ee550ab4bc13c1b7aaa9d3c09c
educacion <educacion@localhost>
2017-09-06 13:37:05 +0000

    Cargado el Driver de mysql

--- Clase12_oop_javaweb_Pablo/src/java/presentacion/Partida.java
+++ Clase12_oop_javaweb_Pablo/src/java/presentacion/Partida.java
@@ -3,6 +3,8 @@ package presentacion;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
@@ -13,30 +15,41 @@ public class Partida extends HttpServlet {
     Gson convertir = new Gson();
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest request, 
+            HttpServletResponse response)
             throws ServletException, IOException {
-        LinkedList listado = new LinkedList();
-        // CCargar el Driver en el DriverMaanger
-        // Conecar a la base
-        // Pedir sentencia pra ejecuar SQL
-        // Ejecutar SQL
-        // Recibir el REsultSet, 
-        //      que es la representacion del CURSOR de la base de datos, 
-        //       del listado del select *
-        // Poder agregar lo elementos de ese ResultSet en la lista resutado
-        // Poder enviar al Navegador Web el JSON representndo esos Datos de la BASE
-        
-        // Instanciar el Objeto Actual,
-        // Alimentandolo con lo que viene de la bse de datos
-        HashMap objetoActual = new HashMap();
+            // CCargar el Driver en el DriverMaanger
+            // Conecar a la base
+            // Pedir sentencia pra ejecuar SQL
+            // Ejecutar SQL
+            // Recibir el REsultSet,
+            //      que es la representacion del CURSOR de la base de datos,
+            //       del listado del select *
+            // Poder agregar lo elementos de ese ResultSet en la lista resutado
+            // Poder enviar al Navegador Web el JSON representndo esos Datos de la BASE
 
-        objetoActual.put("jugada", "peon 4 rey");
-        objetoActual.put("jugador", "Roberto");
-
-        listado.add(objetoActual);
-
-        response.getWriter().println(
-                convertir.toJson(listado));
+        try {
+            LinkedList listado = new LinkedList();
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            
+            
+            // Instanciar el Objeto Actual,
+            // Alimentandolo con lo que viene de la bse de datos
+            HashMap objetoActual = new HashMap();
+            
+            objetoActual.put("jugada", "peon 4 rey");
+            objetoActual.put("jugador", "Roberto");
+            
+            listado.add(objetoActual);
+            
+            response.getWriter().println( convertir.toJson(listado) );
+        } catch (ClassNotFoundException ex) {
+            response.getWriter().println( convertir.toJson("Error: " + ex.getMessage()) );
+        } catch (InstantiationException ex) {
+            response.getWriter().println( convertir.toJson("Error: " + ex.getMessage()) );
+        } catch (IllegalAccessException ex) {
+            response.getWriter().println( convertir.toJson("Error: " + ex.getMessage()) );
+        }
     }
 
     @Override
