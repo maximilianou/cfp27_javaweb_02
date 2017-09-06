2b7eedaa66f56fc97a7785a32586c9d26ac6c725
educacion <educacion@localhost>
2017-09-06 13:09:25 +0000

    Agregado lista y objeto para el JSON

--- Clase12_oop_javaweb_Pablo/src/java/presentacion/Partida.java
+++ Clase12_oop_javaweb_Pablo/src/java/presentacion/Partida.java
@@ -1,20 +1,27 @@
 package presentacion;
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 @WebServlet(name = "Partida", urlPatterns = {"/Partida"})
 public class Partida extends HttpServlet {
-
+    Gson convertir = new Gson();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       response.getWriter().println("GET, Metodo aun no implementado");
+        LinkedList listado = new LinkedList();
+        HashMap objetoActual = new HashMap();
+        listado.add(objetoActual);
+       response.getWriter().println( 
+               convertir.toJson( listado ) );
     }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       response.getWriter().println("POST, Metodo aun no implementado");
+       response.getWriter().println(
+               convertir.toJson( "POST, Metodo aun no implementado" ) );
     }
 }
