bd4e5cb743aa357970566ec6625ce301b209531f
educacion <educacion@localhost>
2017-09-06 13:25:22 +0000

    Info del ToDO que vmos a hacer ahora YAA

--- Clase12_oop_javaweb_Pablo/src/java/presentacion/Partida.java
+++ Clase12_oop_javaweb_Pablo/src/java/presentacion/Partida.java
@@ -16,9 +16,25 @@ public class Partida extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         LinkedList listado = new LinkedList();
+        // CCargar el Driver en el DriverMaanger
+        // Conecar a la base
+        // Pedir sentencia pra ejecuar SQL
+        // Ejecutar SQL
+        // Recibir el REsultSet, 
+        //      que es la representacion del CURSOR de la base de datos, 
+        //       del listado del select *
+        // Poder agregar lo elementos de ese ResultSet en la lista resutado
+        // Poder enviar al Navegador Web el JSON representndo esos Datos de la BASE
+        
+        // Instanciar el Objeto Actual,
+        // Alimentandolo con lo que viene de la bse de datos
         HashMap objetoActual = new HashMap();
 
+        objetoActual.put("jugada", "peon 4 rey");
+        objetoActual.put("jugador", "Roberto");
+
         listado.add(objetoActual);
+
         response.getWriter().println(
                 convertir.toJson(listado));
     }
