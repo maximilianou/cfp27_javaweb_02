9cdd8dc468bd4b9527c05f8342b70828ca862cf0
educacion <educacion@localhost>
2017-09-08 18:56:35 +0000

    agregano ej js

---
+++ Clase12_javaweb_oop_Estela/web/js/helados.js
@@ -0,0 +1,11 @@
+class Heladeria {
+    
+    static pedir(){
+        
+        document.querySelector('#panelOK').innerHTML = "Pedimos Gustos";
+        document.querySelector('#panelError').innerHTML = "En este panel, los errores.";
+        
+    }
+    
+}
+
