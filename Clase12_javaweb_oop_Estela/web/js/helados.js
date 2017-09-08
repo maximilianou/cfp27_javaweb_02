3eabff140f9213c38f1f4d4892c5c53d0a521e69
educacion <educacion@localhost>
2017-09-08 20:52:20 +0000

    select con  base y sabor

--- Clase12_javaweb_oop_Estela/web/js/helados.js
+++ Clase12_javaweb_oop_Estela/web/js/helados.js
@@ -1,11 +1,39 @@
 class Heladeria {
-    
-    static pedir(){
-        
-        document.querySelector('#panelOK').innerHTML = "Pedimos Gustos";
-        document.querySelector('#panelError').innerHTML = "En este panel, los errores.";
-        
+    static consultar() {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'Gusto');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    document.querySelector('#panelResultado').innerHTML 
+                            = xhr.responseText ;
+                    document.querySelector('#panelOK').innerHTML 
+                            = "Consultar Gustos";
+                } else {
+
+                    document.querySelector('#panelError').innerHTML 
+                            = "En este panel, los errores. Por Consultar";
+                }
+            }
+        };
+        xhr.send();
     }
-    
+    static pedir() {
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', 'Gusto');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    document.querySelector('#panelResultado').innerHTML = xhr.responseText ;
+                    document.querySelector('#panelOK').innerHTML = "Pedimos Gustos";
+                } else {
+
+                    document.querySelector('#panelError').innerHTML = "En este panel, los errores. Por Pedir";
+                }
+            }
+        };
+        xhr.send();
+    }
+
 }
 
