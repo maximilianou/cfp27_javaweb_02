438cc47c266598197cb6fa076065a7c72888b907
educacion <educacion@localhost>
2017-09-12 15:28:08 +0000

    Iniciando Viajes

---
+++ Clase13_oop_javaweb_Silvia/web/js/viajes.js
@@ -0,0 +1,12 @@
+"use strict";
+class Viajes {
+    
+    static consultar( elSelector ){
+        
+        document.querySelector("#panelOK").innerHTML = "Funciona con OOP, object oriented programming." ;
+        document.querySelector("#panelERROR").innerHTML = "Cuidado con los Errores." ;
+    }
+    
+}
+
+
