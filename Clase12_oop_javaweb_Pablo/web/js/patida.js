12f779165405a6cd7b7e0c45d128c48abcc946d9
educacion <educacion@localhost>
2017-09-12 15:28:30 +0000

    subiendo

--- Clase12_oop_javaweb_Pablo/web/js/patida.js
+++ Clase12_oop_javaweb_Pablo/web/js/patida.js
@@ -1,6 +1,6 @@
 "use strict";
 class Partida {
-    
+
     static jugar(elSelector){
         let listaInput = document.querySelectorAll(elSelector);
         for( let indice = 0; indice < listaInput.length ; indice++ ){
@@ -10,7 +10,7 @@ class Partida {
         document.querySelector("#panelResultado").innerHTML = JSON.stringify(listaInput);
         document.querySelector("#panelOK").innerHTML = "Funciona con OOP, object oriented programming." ;
         document.querySelector("#panelERROR").innerHTML = "Cuidado con los Errores." ;
-        
+
     }
-    
+
 }
